a84c7abd7da78260a786122feee7dda887d33910
elimchayseng <ethanlimchayseng@Ethans-MacBook-Pro.local>
2019-10-10 11:40:01 -0700

    test db 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_161254) do
+ActiveRecord::Schema.define(version: 2019_10_10_182921) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,8 +32,15 @@ ActiveRecord::Schema.define(version: 2019_10_08_161254) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "zmail", default: "", null: false
+    t.string "zncrypted_password", default: "", null: false
+    t.string "zreset_password_token"
+    t.datetime "zreset_password_sent_at"
+    t.datetime "zremember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["zreset_password_token"], name: "index_users_on_zreset_password_token", unique: true
+    t.index [nil], name: "index_users_on_zemail", unique: true
   end
 
 end
