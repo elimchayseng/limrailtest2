fde7ad5904e639a9092266f02b49e5c8c612f247
elimchayseng <ethanlimchayseng@Ethans-MacBook-Pro.local>
2019-10-11 17:37:31 -0700

    added authentication for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_11_202826) do
+ActiveRecord::Schema.define(version: 2019_10_11_224615) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_10_11_202826) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
