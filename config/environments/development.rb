3883d4e1e85ab942d819efdcc3831d973c2c49dd
elimchayseng <ethanlimchayseng@Ethans-MacBook-Pro.local>
2019-10-22 12:02:50 -0700

    aws install

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,8 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  #config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -63,5 +64,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 # Added per devise installation instructions 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 
 end
